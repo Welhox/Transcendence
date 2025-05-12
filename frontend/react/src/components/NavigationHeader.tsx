f2923d05db28dbc80579753e9ed205074788e9b1
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-12 11:59:12 +0300

    Appropriate menu items show when logged in vs logged out on main page

--- frontend/react/src/components/NavigationHeader.tsx
+++ frontend/react/src/components/NavigationHeader.tsx
@@ -1,25 +1,35 @@
-const NavigationHeader = ({handleStats, handlePals, handleSettings, logout}) => {
+const NavigationHeader = ({handleStats, handlePals, handleSettings, logout, handleLogin, status}) => {
 	const handleReturn = () => {
 		navigate('/');
 	}
 	const NavButton = ({insideText, handler}) => {
-		return <button className="border bg-teal-500 font-semibold hover:font-extrabold 
-					  hover:underline uppercase text-white p-4 mx-4 rounded-2xl" onClick={handler}>{insideText}</button>
+		return <button className="border bg-teal-900 font-semibold hover:font-extrabold 
+					  hover:underline hover:text-amber-200 uppercase text-white p-4 mx-4 rounded-2xl" onClick={handler}>{insideText}</button>
 	}
 	const LoginButton = ({insideText, handler}) => {
-		return <button className="border bg-amber-900 font-semibold hover:font-extrabold 
-					  hover:underline uppercase text-white p-4 mx-4 rounded-2xl" onClick={handler}>{insideText}</button>
+		return <button className="border bg-teal-500 font-semibold hover:font-extrabold 
+					  hover:underline  hover:text-amber-200 uppercase text-white p-4 mx-4 
+					  rounded-2xl" onClick={handler}>{insideText}</button>
 	
 	}
-	return (
-	<nav class="flex justify-between bg-teal-800 p-2">
+	if (status === 'authorized')
+		return (
+	<nav className="flex justify-between bg-teal-700 p-2">
 		<NavButton insideText="Home" handler={handleReturn}></NavButton>
 		<NavButton insideText="My Stats" handler={handleStats}></NavButton>
 		<NavButton insideText="Pong pals" handler={handlePals}></NavButton>
 		<NavButton insideText="Settings" handler={handleSettings}></NavButton>
-		<LoginButton insideText="Login" handler={logout}></LoginButton>
+
+		<LoginButton insideText="Logout" handler={logout}></LoginButton>
 	</nav>
 )
+	else
+		return(
+			<nav className="flex justify-between bg-teal-700 p-2">
+			<NavButton insideText="Home" handler={handleReturn}></NavButton>
+			<LoginButton insideText="Login" handler={handleLogin}></LoginButton>
+			</nav>
+		)
 };
 
 export default NavigationHeader;
--- frontend/react/src/pages/Home.tsx
+++ frontend/react/src/pages/Home.tsx
@@ -43,9 +43,12 @@ const Home: React.FC = () => {
 			<NavigationHeader handleStats={handleStats}
 									  handlePals={handlePals}
 									  handleSettings={handleSettings}
-									  logout={logout} />
-			<div class="flex justify-center"><img class="object-contain max-h-full m-auto" src="assets/pong-placeholder.gif"></img></div>
-			<h1 class="text-6xl text-center text-teal-800 m-3">Welcome!</h1>
+									  logout={logout}
+									  handleLogin={handleLogin}
+									  status={status}
+									  />
+			<div className="flex justify-center"><img className="object-contain max-h-full m-auto" src="assets/pong-placeholder.gif"></img></div>
+			<h1 className="text-6xl text-center text-teal-800 m-3">Welcome!</h1>
 
 			{status === 'loading' ? (
 				<p>Checkin session...</p>
@@ -58,10 +61,10 @@ const Home: React.FC = () => {
 				</>
 			) : (
 				<>
-					<p class="text-center">Please log in to access exclusive Pong content and connect with other registered players!</p>
-					{/*<button class="border bg-amber-900 font-semibold hover:font-extrabold
-					  hover:underline uppercase text-white p-4 mx-4 rounded-2xl" onClick={handleLogin}>Login</button>*/}
-					<p class="text-center">No account?{' '} <Link class="text-amber-900 font:semi-bold hover:font-extrabold" to="/register">Register</Link></p>
+					<p className="text-center">Please log in to access exclusive Pong content and connect with other registered players!</p>
+					<button className="border bg-amber-900 font-semibold hover:font-extrabold
+					  hover:underline uppercase text-white p-4 mx-4 rounded-2xl" onClick={handleLogin}>Login</button>
+					<p className="text-center">No account?{' '} <Link className="text-amber-900 font:semi-bold hover:font-extrabold" to="/register">Register</Link></p>
 				</>
 			)}
 		</div>
