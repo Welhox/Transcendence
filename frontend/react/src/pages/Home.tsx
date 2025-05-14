b3da3bc9605a5bb7165b32f9a9a018ad6cc9339e
Ryan Boudwin <rboudwin@student.hive.fi>
2025-05-14 16:43:15 +0300

    Home and Register pages now styled for dark mode

--- frontend/react/src/pages/Home.tsx
+++ frontend/react/src/pages/Home.tsx
@@ -10,18 +10,18 @@ const Home: React.FC = ({status, user}) => {
 	return (
 		<div>
 			<div className="flex justify-center"><img className="object-contain max-h-full m-auto" src="assets/pong-placeholder.gif"></img></div>
-			<h1 className="text-6xl text-center text-teal-800 m-3">Welcome!</h1>
+			<h1 className="text-6xl text-center text-teal-800 dark:text-teal-300 m-3">Welcome!</h1>
 
 			{status === 'loading' ? (
 				<p>Checkin session...</p>
 			) : status === 'authorized' && user ? (
 				<>
-					<p>Hello, {user.username}</p>
+					<p className="dark:text-white">Hello, {user.username}</p>
 				</>
 			) : (
 				<>
-					<p className="text-center">Please log in to access exclusive Pong content and connect with other registered players!</p>
-					<p className="text-center">No account?{' '} <Link className="text-amber-900 font:semi-bold hover:font-extrabold" to="/register">Register</Link></p>
+					<p className="dark:text-white text-center">Please log in to access exclusive Pong content and connect with other registered players!</p>
+					<p className="dark:text-white text-center">No account?{' '} <Link className="text-amber-900 dark:text-amber-300 font:semi-bold hover:font-extrabold" to="/register">Register</Link></p>
 				</>
 			)}
 		</div>
--- frontend/react/src/pages/Register.tsx
+++ frontend/react/src/pages/Register.tsx
@@ -80,7 +80,7 @@ const Register: React.FC = () => {
 	const inputStyles = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 	return (
     	<div>
-			<h1 className="text-6xl text-center text-teal-800 m-3">Create an account</h1>
+			<h1 className="text-6xl text-center text-teal-800 dark:text-teal-300 m-3">Create an account</h1>
 			<div>
 			<form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
 				<div className="mb-5"><label className={labelStyles} htmlFor="username">Username: </label>
@@ -135,11 +135,18 @@ const Register: React.FC = () => {
 					<p style={{ color: 'red', marginTop: '8px'}}>{errorMessage}</p>
 				)}
 
-				<button className="block mx-auto px-20 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="submit">Register</button>
+				<button className="block mx-auto px-20 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 
+								  focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full 
+								  sm:w-auto py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700
+								  dark:focus:ring-teal-800" type="submit">Register</button>
 			</form></div>
 
-			<p className="m-5 text-center">
-				Already have an account? <Link className="text-white bg-amber-700 hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mx-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" to="/login">Login</Link>
+			<p className="m-5 text-center dark:text-white">
+				Already have an account? <Link className="text-white bg-amber-700 hover:bg-amber-800 
+										 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium 
+										 rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 mx-3 text-center
+										  dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
+										  to="/login">Login</Link>
 			</p>
 		</div>
 	)
