ef58a30ef9a00ea8f53c92bac7955e6c74aa6556
Ryan Boudwin <rboudwin@student.hive.fi>
2025-06-12 14:11:15 +0300

    Added choose play mode component

---
+++ frontend/react/src/components/ChoosePlayMode.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import PongGameWithRegistration from "./PongGameWithRegistration";
+
+const ChoosePlayMode = () => {
+  const [selectedMode, setSelectedMode] = useState<string | null>(null);
+  useEffect(() => {
+    if (selectedMode) {
+      console.log("selectedMode: ", selectedMode);
+    }
+  }, [selectedMode]);
+  const buttonStyles =
+    "px-5 mx-3 py-5 my-2 text-white bg-teal-700 hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-lg text-sm w-full sm:w-auto py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800";
+
+  const handleSinglePlayer = () => {
+    setSelectedMode("single-player");
+  };
+
+  const handleTwoPlayer = () => {
+	setSelectedMode("two-player-single-game");
+  }
+
+  const handleCreateTournament = () => {
+	setSelectedMode("create-tournament");
+  }
+
+  const handleJoinTournament = () => {
+	setSelectedMode("join-tournament");
+  }
+  return (
+    <div>
+      {selectedMode  === 'two-player-single-game' ? (
+        <PongGameWithRegistration />
+      ) : (
+        <div>
+          <h1 className="text-6xl text-center text-teal-800 dark:text-teal-300 m-3">
+            Choose Play Mode
+          </h1>
+          <div className="flex">
+            <button className={buttonStyles} onClick={handleSinglePlayer}>Single Player vs AI</button>
+            <button className={buttonStyles} onClick={handleTwoPlayer}>2 Player Single Game</button>
+            <button className={buttonStyles} onClick={handleCreateTournament}>Create Tournament</button>
+            {/* <button className={buttonStyles} onClick={handleJoinTournament}>Join Tournament</button> */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+export default ChoosePlayMode;
--- frontend/react/src/pages/Home.tsx
+++ frontend/react/src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { User, AuthContextType } from '../auth/AuthProvider';
-import NavigationHeader from '../components/NavigationHeader';
-import PongGame from '../components/PongGame';
-import PongGameWithRegistration from '../components/PongGameWithRegistration';
-
-import { useTranslation } from 'react-i18next';
-import i18n from '../i18n';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { User, AuthContextType } from "../auth/AuthProvider";
+import NavigationHeader from "../components/NavigationHeader";
+import PongGame from "../components/PongGame";
+import PongGameWithRegistration from "../components/PongGameWithRegistration";
+import { useTranslation } from "react-i18next";
+import i18n from "../i18n";
+import ChoosePlayMode from "../components/ChoosePlayMode";
 
 interface HomeProps {
   status: AuthContextType["status"];
@@ -14,35 +14,48 @@ interface HomeProps {
 }
 
 const Home: React.FC<HomeProps> = ({ status, user }) => {
-  	const { t } = useTranslation();
+  const { t } = useTranslation();
 
-	useEffect(() => {
-    	if (status !== 'authorized') {
-    		i18n.changeLanguage('en');
-    	}
-	}, [status, user]);
+  useEffect(() => {
+    if (status !== "authorized") {
+      i18n.changeLanguage("en");
+    }
+  }, [status, user]);
 
-	return (
-		<div className="text-center max-w-2xl dark:bg-black bg-white mx-auto rounded-lg my-5">
-			<div className="flex justify-center">
-				<PongGameWithRegistration />
-			</div>
-			<h1 className="text-6xl text-center text-teal-800 dark:text-teal-300 m-3">{t('home.welcome')}</h1>
+  return (
+    <div className="text-center max-w-2xl dark:bg-black bg-white mx-auto rounded-lg my-5">
+      <div className="flex justify-center">
+        <ChoosePlayMode />
+        {/* <PongGameWithRegistration /> */}
+      </div>
+      <h1 className="text-6xl text-center text-teal-800 dark:text-teal-300 m-3">
+        {t("home.welcome")}
+      </h1>
 
-			{status === 'loading' ? (
-				<p>{t('home.checkingSession')}</p>
-			) : status === 'authorized' && user ? (
-				<>
-					<p className="dark:text-white pb-10">{t('home.hello')}, {user.username}</p>
-				</>
-			) : (
-				<>
-					<p className="dark:text-white text-center">{t('home.pleaseLogin')}</p>
-					<p className="dark:text-white text-center font-bold p-5">{t('home.noAccount')}{' '} <Link className="text-amber-900 dark:text-amber-300 font:bold hover:font-extrabold" to="/register">{t('home.register')}</Link></p>
-				</>
-			)}
-		</div>
-	);
+      {status === "loading" ? (
+        <p>{t("home.checkingSession")}</p>
+      ) : status === "authorized" && user ? (
+        <>
+          <p className="dark:text-white pb-10">
+            {t("home.hello")}, {user.username}
+          </p>
+        </>
+      ) : (
+        <>
+          <p className="dark:text-white text-center">{t("home.pleaseLogin")}</p>
+          <p className="dark:text-white text-center font-bold p-5">
+            {t("home.noAccount")}{" "}
+            <Link
+              className="text-amber-900 dark:text-amber-300 font:bold hover:font-extrabold"
+              to="/register"
+            >
+              {t("home.register")}
+            </Link>
+          </p>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
