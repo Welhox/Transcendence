e22155d2bb20bea8494b31baa37613efacaa65ce
Ryan Boudwin <rboudwin@student.hive.fi>
2025-07-18 11:56:29 +0300

    Set max length of player names in game component and tournamentbuilder

--- frontend/react/src/components/PlayerRegistrationBox.tsx
+++ frontend/react/src/components/PlayerRegistrationBox.tsx
@@ -218,6 +218,7 @@ const PlayerRegistrationBox: React.FC<PlayerBoxProps> = ({
           placeholder={t("playerBox.usernamePlaceholder")}
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+		  maxLength={20}
         />
         <input
           className="mb-2 p-2 border rounded w-48"
@@ -225,6 +226,7 @@ const PlayerRegistrationBox: React.FC<PlayerBoxProps> = ({
           placeholder={t("playerBox.passwordPlaceholder")}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+		  maxLength={42}
         />
         <button
           className="bg-teal-700 text-white px-4 py-2 rounded"
